1a9a02f5f2ad9f71f21bf4186f3037ec84ecbdce
Luis René Panjón <luchorene97@gmail.com>
2022-03-03 14:04:38 -0500

    feat(Root): Añadir Feed de videos y ajustar scroll

---
+++ src/components/FeedVideos/index.jsx
@@ -0,0 +1,39 @@
+import { VideoPlayer } from '../VideoPlayer'
+import styles from './styles.module.css'
+
+const VIDEOS = [
+  {
+    id: 1,
+    author: 'francoescamillaoficial',
+    description: 'Cuando retas a tu amigo y el no se ahueva 😎😎 #rap #freestyle',
+    likes: 121,
+    shares: 283,
+    comment: 94,
+    songTitle: 'sonido original - Franco Escamilla',
+    albumCover: 'https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/f690e67830de443a35d9188b5c964309.jpeg?x-expires=1646344800&x-signature=QUkic2Glk%2BUcUP%2BWUduCD%2BEIUb4%3D',
+    src: 'https://v16-webapp.tiktok.com/91f1d2d9d4e7c2d236dcc9409bfe7cdb/621ff847/video/tos/useast2a/tos-useast2a-ve-0068c004/2c6af9b314d743e0a2460cfb0420201f/?a=1988&br=2858&bt=1429&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7ThLrccDXq&l=202203021705190101921680910C4768C9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3RtdWc6ZmVrOjMzNzczM0ApaTk1NWY0ZWVoNzVmPDdmZGdgaDRzcjRvM3NgLS1kMTZzczQwNTI1Ll8uMTBgYjNjXy46Yw%3D%3D&vl=&vr='
+  },
+  {
+    id: 2,
+    author: 'videossheldon',
+    description: 'Cuando prefieres perder a que pedir ayuda 😅😅 #TheBigBangTheory #Sheldon',
+    likes: 163,
+    shares: 218,
+    comment: 102,
+    songTitle: 'sonido original - P.D.M.81',
+    albumCover: 'https://lf16-tiktok-web.ttwstatic.com/obj/tiktok-web-us/tiktok/web/node/_next/static/images/album-628a1f27c2ed684e4454d53f4c042f7a.svg',
+    src: 'https://v16-webapp.tiktok.com/c9162ff6ea068b8473b25dc0be8f46ec/622040bc/video/tos/useast2a/tos-useast2a-ve-0068c004/638e807a36d840899eb5d91ac71a8414/?a=1988&br=830&bt=415&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7Th~gdcDXq&l=2022030222133801022308301505118CD4&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzx0bzY6ZnhxNzMzNzczM0ApaDZlNTY3NTs6NzQ2PDk0ZGdgXl9ecjRvMjVgLS1kMTZzczViM141YDZjNDI1XzE2NTQ6Yw%3D%3D&vl=&vr='
+  }
+]
+
+export const FeedVideos = () => {
+  return (
+    VIDEOS.map((video) => {
+      return (
+        <div key={video.id} className={styles.item}>
+          <VideoPlayer src={video} />
+        </div>
+      )
+    })
+  )
+}
--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,30 +1,31 @@
+import clsx from 'clsx'
 import { useRef, useState } from 'react'
 import styles from './styles.module.css'
 
-const SRC = 'https://v16-webapp.tiktok.com/91f1d2d9d4e7c2d236dcc9409bfe7cdb/621ff847/video/tos/useast2a/tos-useast2a-ve-0068c004/2c6af9b314d743e0a2460cfb0420201f/?a=1988&br=2858&bt=1429&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7ThLrccDXq&l=202203021705190101921680910C4768C9&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3RtdWc6ZmVrOjMzNzczM0ApaTk1NWY0ZWVoNzVmPDdmZGdgaDRzcjRvM3NgLS1kMTZzczQwNTI1Ll8uMTBgYjNjXy46Yw%3D%3D&vl=&vr='
-export const VideoPlayer = () => {
+export const VideoPlayer = ({ src }) => {
   const [playing, setPlaying] = useState(false)
-  const video = useRef()
+  const video = useRef(null)
 
   const handlePlay = () => {
-    if (playing) {
-      video.current.pause()
-      setPlaying(!playing)
-    } else {
-      video.current.play()
-      setPlaying(!playing)
-    }
+    playing ? video.current.pause() : video.current.play()
+    setPlaying(!playing)
   }
 
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing
+  })
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       <video
-        src={SRC}
-        controls={false}
         className={styles.video}
+        controls={false}
+        loop
+        onClick={handlePlay}
         ref={video}
+        src={src.src}
       />
-      <button className={styles.player} onClick={handlePlay} />
+      <i className={playerClassName} onClick={handlePlay} />
     </div>
   )
 }

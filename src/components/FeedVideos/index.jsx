8874fff301fd229f8033b612c94f7b21c528c065
Luis René Panjón <luchorene97@gmail.com>
2022-03-04 16:06:28 -0500

    feat(Root): Añadir svg de iconos de botones de acción

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -11,7 +11,7 @@ const VIDEOS = [
     comment: 94,
     songTitle: 'sonido original - Franco Escamilla',
     albumCover: 'https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/f690e67830de443a35d9188b5c964309.jpeg?x-expires=1646344800&x-signature=QUkic2Glk%2BUcUP%2BWUduCD%2BEIUb4%3D',
-    src: 'https://v16-webapp.tiktok.com/053674d747dcab3aeceaaf1acc150cd1/62216a1b/video/tos/useast2a/tos-useast2a-ve-0068c004/2c6af9b314d743e0a2460cfb0420201f/?a=1988&br=2858&bt=1429&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7ThFSnCDXq&l=20220303192315010223087161063EC2AF&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3RtdWc6ZmVrOjMzNzczM0ApaTk1NWY0ZWVoNzVmPDdmZGdgaDRzcjRvM3NgLS1kMTZzczQwNTI1Ll8uMTBgYjNjXy46Yw%3D%3D&vl=&vr='
+    src: 'https://v16-webapp.tiktok.com/e675cabf2cce8790218e09f2025b16ab/6222b715/video/tos/useast2a/tos-useast2a-ve-0068c004/2c6af9b314d743e0a2460cfb0420201f/?a=1988&br=2858&bt=1429&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7Th7vbCDXq&l=202203041903570101890650801F51EEF5&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3RtdWc6ZmVrOjMzNzczM0ApaTk1NWY0ZWVoNzVmPDdmZGdgaDRzcjRvM3NgLS1kMTZzczQwNTI1Ll8uMTBgYjNjXy46Yw%3D%3D&vl=&vr='
   },
   {
     id: 2,
@@ -22,7 +22,7 @@ const VIDEOS = [
     comment: 102,
     songTitle: 'sonido original - P.D.M.81',
     albumCover: 'https://lf16-tiktok-web.ttwstatic.com/obj/tiktok-web-us/tiktok/web/node/_next/static/images/album-628a1f27c2ed684e4454d53f4c042f7a.svg',
-    src: 'https://v16-webapp.tiktok.com/12a17ea1533217776dfe91702ed9f7e4/62216af5/video/tos/useast2a/tos-useast2a-ve-0068c004/638e807a36d840899eb5d91ac71a8414/?a=1988&br=830&bt=415&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7ThctnCDXq&l=20220303192603010223084133033EF160&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzx0bzY6ZnhxNzMzNzczM0ApaDZlNTY3NTs6NzQ2PDk0ZGdgXl9ecjRvMjVgLS1kMTZzczViM141YDZjNDI1XzE2NTQ6Yw%3D%3D&vl=&vr='
+    src: 'https://v16-webapp.tiktok.com/db770e89138c2b1388afac5f5d71bc84/6222b79f/video/tos/useast2a/tos-useast2a-ve-0068c004/638e807a36d840899eb5d91ac71a8414/?a=1988&br=830&bt=415&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-30-nz7ThJ2bCDXq&l=2022030419052501022307203527548E10&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=Mzx0bzY6ZnhxNzMzNzczM0ApaDZlNTY3NTs6NzQ2PDk0ZGdgXl9ecjRvMjVgLS1kMTZzczViM141YDZjNDI1XzE2NTQ6Yw%3D%3D&vl=&vr='
   }
 ]
 
--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,16 +1,23 @@
+import { Comment } from '../Icons/Comment'
 import { Heart } from '../Icons/Heart'
+import { Share } from '../Icons/Share'
 import styles from './styles.module.css'
-export const VideoPlayerActions = () => {
+export const VideoPlayerActions = ({ likes = 125, comments = 59, shares = 124, hearted = false }) => {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='like'>{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title='comments'>{comments}</strong>
+
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='shares'>{shares}</strong>
+
       </div>
     </aside>
   )
